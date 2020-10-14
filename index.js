8cadded9fbc4e86e505a9fe6c48c1b623d7a785d
Tanner Kirkpatrick <twk4491@gmail.com>
2020-10-14 14:31:22 -0400

    licenses added

--- index.js
+++ index.js
@@ -27,14 +27,20 @@ function promptUser(){
             name: "usage"
         },
         {
+            type: "list",
+            message: "Which license(s) would you like?",
+            choices: ["Apache License 2.0", "BSD 3-Clause 'New' or 'Revised' license", "BSD 2-Clause 'Simplified' or 'FreeBSD' license", "GNU General Public License (GPL)", "GNU Library or 'Lesser' General Public License (LGPL)", "MIT license", "Mozilla Public License 2.0", "Common Development and Distribution License", "Eclipse Public License version 2.0"],
+            name: "license"
+        },
+        {
             type: "input",
             message: "Enter guidelines for how to contribute to this project:",
-            name: "contribution"
+            name: "contributions"
         },
         {
             type: "input",
             message: "Enter test instructions for your project:",
-            name: "test"
+            name: "tests"
         },
         {
             type: "input",
@@ -57,13 +63,30 @@ ${answers.description}
 ## Table of Contents
 * [Installation](#installation)
 * [Usage](#usage)
+* [License](#license)
 * [Contributions](#contributions)
 * [Test](#test)
 * [Questions](#questions)
-* [License](#license)
+
 
 ## Installation
 ${answers.installation}
+
+## Usage
+${answers.usage}
+
+## License
+${answers.license}
+
+## Contributions
+${answers.contributions}
+
+## Tests
+${answers.tests}
+
+## Questions
+${answers.github}
+${answers.email}
     
     
     
